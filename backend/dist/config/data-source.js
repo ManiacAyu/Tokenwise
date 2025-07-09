5f2322af99969a9c848fdae93ea5c3ba18dced83
ManiacAyu <Ayush Dhoke>
2025-07-09 14:48:54 +0530

    connecting to remote database server

--- backend/dist/config/data-source.js
+++ backend/dist/config/data-source.js
@@ -1,15 +1,17 @@
 import { DataSource } from 'typeorm';
 import { Wallet } from '../models/Wallet.js';
 import { Transaction } from '../models/Transaction.js';
+
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'ayush',
-    database: 'tokenwise',
+    url: 'postgresql://neondb_owner:npg_kugCbfB0r6oE@ep-green-frog-a1f9j2xy-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
     synchronize: true,
     logging: false,
+    ssl: true,
+    extra: {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    },
     entities: [Wallet, Transaction],
 });
-//# sourceMappingURL=data-source.js.map
