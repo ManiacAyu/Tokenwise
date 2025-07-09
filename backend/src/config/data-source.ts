aaa248dc6c7799b5dea375aeb5cf9ebbfa2c820b
ManiacAyu <Ayush Dhoke>
2025-07-09 15:43:43 +0530

    added remote db

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -3,13 +3,15 @@ import { Wallet } from '../models/Wallet.js';
 import { Transaction } from '../models/Transaction.js';
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'ayush',
-  database: 'tokenwise',
-  synchronize: true,
-  logging: false,
-  entities: [Wallet, Transaction],
+    type: 'postgres',
+    url: 'postgresql://neondb_owner:npg_kugCbfB0r6oE@ep-green-frog-a1f9j2xy-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+    synchronize: true,
+    logging: false,
+    ssl: true,
+    extra: {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    },
+    entities: [Wallet, Transaction],
 });
