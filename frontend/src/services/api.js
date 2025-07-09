8072a75e3d3b71baee0b63596eb5dee07885bc8b
ManiacAyu <Ayush Dhoke>
2025-07-09 15:52:48 +0530

    updating backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://tokenwise-7msi.onrender.com/api',
 });
 
 export const fetchTransactions = () => API.get('/transactions');
